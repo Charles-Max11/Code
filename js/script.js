cc421a4e70db35add721666afad074edce4c3433
Charles <charlesmax.delamotte@gmail.com>
2025-03-21 22:11:16 +0100

    Masquer connexion/deco en fonction du statut

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const tokenCookieName = "accesstoken";
+const RoleCookieName = "role";
 const signoutBTN = document.getElementById("signout-btn");
 
 signoutBTN.addEventListener("click", signout);
 
+function getRole(){
+    return getCookie(RoleCookieName);
+}
+
 function signout(){
     eraseCookie(tokenCookieName);
+    eraseCookie(RoleCookieName);
     window.location.reload();
 }
 
@@ -50,9 +56,34 @@ function isConnected(){
     }
 }
 
-if(isConnected()){
-    alert('je suis connecté')
-}
-else {
-    alert('Je ne suis pas connecté')
+function showAndHideElementsForRoles() {
+    const userConnected = isConnected();
+    const role = getRole();
+
+    let allElementsToEdit = document.querySelectorAll('[data-show]');
+
+    allElementsToEdit.forEach(element =>{
+        switch(element.dataset.show){
+            case 'disconnected':
+                if(userConnected){
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'connected':
+                if(!userConnected){
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'admin':
+                if(!userConnected || role != "admin"){
+                    element.classList.add("d-none");
+                }
+                break;
+            case 'client':
+                if(!userConnected || role != "client"){
+                    element.classList.add("d-none");
+                }
+                break;
+        }
+    })
 }
